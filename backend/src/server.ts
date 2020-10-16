0e3873f394e7021880cfb84de04620f294408fcb
luizfverissimo <62751844+luizfverissimo@users.noreply.github.com>
2020-10-15 21:26:52 -0300

    aula2 METADE

---
+++ backend/src/server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connections';
+
+const app = express();
+app.use(express.json());
+
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = req.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanagesRepository.save(orphanage)
+
+  return res.status(200).json(orphanage)
+});
+
+app.listen(3333);
